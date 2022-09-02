b8010cc7adf7da3e5a4a0ea64e153d7e379cf1b1
Tanin Rahman <taninr87@gmail.com>
2022-09-03 04:17:23 +0600

    add modal on details button

--- scripts/script.js
+++ scripts/script.js
@@ -30,6 +30,7 @@ const loadCategoriesNews = (id) => {
 }
 const displayCategoriesNews = news => {
   console.log(news);
+  news.sort((a,b )=> b.total_view - a.total_view);
   const showNewsNumber = document.getElementById("news-number");
   showNewsNumber.textContent = `${news.length} items Found!`;
   const displayNews = document.getElementById("display-categories-news");
@@ -51,14 +52,14 @@ const displayCategoriesNews = news => {
         <div class="d-flex justify-content-between align-items-center mt-5 align-self-bottom">       
               <div class="d-flex align-items-center">
                   <img class="author-img me-2" src="${element.author.img ? element.author.img : "No image Found"}">
-                  <p>${element.author.name === "system" ? "No Author Found" : element.author.name}</p>
+                  <p>${element.author.name ?  element.author.name : "No Author Found"}</p>
               </div>
               <div class="total-view d-flex align-items-center">
                   <i class="fa fa-eye me-3"></i>
                   <p class="m-0">${element.total_view ? element.total_view : "No Views"}</p>
               </div>
               <div class="details-btn">
-                <button class="btn btn-danger">Show Details</button>
+                <button onclick="loadNewsDetails('${element._id}')" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#newsDetailsModal">Show Details</button>
               </div>
        </div>
     </div>`;
@@ -66,6 +67,28 @@ const displayCategoriesNews = news => {
   loader(false);
   })
 }
+
+// Load News Details By Modal
+const loadNewsDetails = news_id => {
+  fetch(`https://openapi.programming-hero.com/api/news/${news_id}`)
+  .then(res => res.json())
+  .then(data => displayNewsDetails(data.data[0]))
+  .catch(error => console.log(error));
+}
+const displayNewsDetails = news => {
+  console.log(news);
+  const modalTitle = document.getElementById("newsDetailsModalLabel");
+  const modalDetails = document.getElementById("modal-details");
+  const modalAuthor = document.getElementById("author");
+  const modalPublishDate = document.getElementById("publish-date");
+  const modalImg = document.getElementById("modal-img");
+  modalPublishDate.textContent = `Published Date: ${news.author.published_date ? news.author.published_date : "No Author Found!"}`
+  modalAuthor.textContent = `Author Name: ${news.author.name ? news.author.name : "No Author Found!"}`;
+  modalImg.src = `${news.image_url ? news.image_url : "No image found!"}`;
+  modalDetails.textContent = news.details.slice(0,300);
+  modalTitle.textContent = news.title;
+}
+
 
 // Spinners Function
 const loader = isLoading => {
