d230c360956a197ef1bf4d0437c4caaae4304b7f
Tanin Rahman <taninr87@gmail.com>
2022-09-03 18:28:10 +0600

    delete accordian and create blog

---
+++ scripts/blog.js
@@ -0,0 +1,16 @@
+const blogsContainer = document.getElementById("blogs-container");
+const firstBlogTitle = document.getElementById("first-blog-title");
+const secondBlogTitle = document.getElementById("second-blog-title");
+const thirdBlogTitle = document.getElementById("third-blog-title");
+const firstBlogPara = document.getElementById("first-blog-para");
+const secondBlogPara = document.getElementById("second-blog-para");
+const thirdBlogPara = document.getElementById("third-blog-para");
+
+firstBlogTitle.textContent = "Difference between var, let, and const?";
+firstBlogPara.textContent = "Var variable can be re-declared and updated.But let variable can not be re-declared but can be updated.and const is constant this can't be change.The scope of a var variable is functional scope. The scope of a let variable is block scope. The scope of a const variable is block scope. It can be updated and re-declared into the scope.";
+
+secondBlogTitle.textContent = "What is defference between Arrow Function and Regular Function?";
+secondBlogPara.textContent = "Unlike regular functions, arrow functions do not have their own this . The value of this inside an arrow function remains the same throughout the lifecycle of the function and is always bound to the value of this in the closest non-arrow parent function.it's also called anonymous function.";
+
+thirdBlogTitle.textContent = "What is Difference map(), forEach(), filter(), find()?";
+thirdBlogPara.textContent = "The forEach() method does not create a new array based on the given array. The map() method creates an entirely new array. The forEach() method returns “undefined“. The map() method returns the newly created array according to the provided callback function.The Filter method give a new with the matching conditions.and The Filter method gives only the first matched element of an array.";
--- scripts/script.js
+++ scripts/script.js
@@ -1,109 +1,128 @@
 // Load Categories
 const loadCategories = () => {
   fetch("https://openapi.programming-hero.com/api/news/categories")
-  .then(res => res.json())
-  .then(data => displayCategories(data.data.news_category))
-  .catch(error => console.log(error));
-}
+    .then((res) => res.json())
+    .then((data) => displayCategories(data.data.news_category))
+    .catch((error) => console.log(error));
+};
 // Display Categories
 const displayCategories = (categories) => {
   const newsNumberCounter = document.getElementById("news-number-counter");
   const listOfCategories = document.getElementById("list-of-categories");
-  categories.forEach(element => {
+  categories.forEach((element) => {
     const category = document.createElement("li");
     category.innerText = element.category_name;
     category.addEventListener("click", (e) => {
       loader(true);
       newsNumberCounter.classList.remove("d-none");
-      loadCategoriesNews(element.category_id,e.target);
+      loadCategoriesNews(element.category_id, e.target);
       // e.target.style.color = "red";
-    })
-    listOfCategories.appendChild(category);   
+    });
+    listOfCategories.appendChild(category);
   });
-}
+};
 
 // Load Categories News
-const loadCategoriesNews = (id,targetCategory) => {
+const loadCategoriesNews = (id, targetCategory) => {
   fetch(`https://openapi.programming-hero.com/api/news/category/${id}`)
-  .then(res => res.json())
-  .then(data => displayCategoriesNews(data.data,targetCategory))
-  .catch(error => console.log(error));
-}
+    .then((res) => res.json())
+    .then((data) => displayCategoriesNews(data.data, targetCategory))
+    .catch((error) => console.log(error));
+};
 
 // Display categories News
-const displayCategoriesNews = (news,targetCategory) => {
-  news.sort((a,b )=> b.total_view - a.total_view);
+const displayCategoriesNews = (news, targetCategory) => {
+  news.sort((a, b) => b.total_view - a.total_view);
   const showNewsNumber = document.getElementById("news-number");
-  if(news.length === 0){
-    showNewsNumber.textContent = `No items Found for category ${targetCategory.textContent}!`;
+  if (news.length === 0) {
+    showNewsNumber.textContent = `No Items Found For Categor ${targetCategory.textContent}!`;
   } else {
-    showNewsNumber.textContent = `${news.length} items Found for category ${targetCategory.textContent}!`;
+    showNewsNumber.textContent = `${news.length} Items Found For Category ${targetCategory.textContent}!`;
   }
- const displayNews = document.getElementById("display-categories-news");
-  displayNews.textContent = "";
-  news.forEach(element => {
-    const newsDiv = document.createElement("div");
-    newsDiv.classList.add("d-flex");
-    newsDiv.classList.add("news");
-    newsDiv.innerHTML = `
-    <div class="">
+  const displayNews = document.getElementById("display-categories-news");
+  if (news.length === 0) {
+    displayNews.innerHTML = `<h1 class="text-center text-warning mt-4">No News Found!</h1>`;
+    loader(false);
+  } else {
+    displayNews.textContent = "";
+    news.forEach((element) => {
+      const newsDiv = document.createElement("div");
+      newsDiv.classList.add("d-flex");
+      newsDiv.classList.add("news");
+      newsDiv.innerHTML = `
+    <div>
         <img src="${element.thumbnail_url}" alt="">
     </div>
     <div class="p-3 d-flex flex-column">
        <div>
        <h3>${element.title}</h3>
-       <p>${element.details.slice(0,250).concat("...")}</p>
+       <p>${element.details.slice(0, 250).concat("...")}</p>
        </div>
-        <div class="author-section d-flex justify-content-between align-items-center mt-5 align-self-bottom">       
+        <div class="author-section d-flex justify-content-between align-items-center align-self-bottom">       
               <div class="d-flex align-items-center">
-                  <img class="author-img me-2 img-fluid" src="${element.author.img ? element.author.img : "No image Found"}">
+                  <img class="author-img me-2 img-fluid" src="${
+                    element.author.img ? element.author.img : "No image Found"
+                  }">
                   <div class="mt-2">
-                  <h5 class="mb-0">${element.author.name ?  element.author.name : "No Author Found"}</h5>
+                  <h5 class="mb-0">${
+                    element.author.name
+                      ? element.author.name
+                      : "No Author Found"
+                  }</h5>
                   <p class="mt-0">${element.author.published_date}</p>
                   </div>
               </div>
               <div class="total-view d-flex align-items-center">
                   <i class="fa fa-eye me-3"></i>
-                  <p class="m-0">${element.total_view ? element.total_view : "No Views"}</p>
+                  <p class="m-0">${
+                    element.total_view ? element.total_view : "No Views"
+                  }</p>
               </div>
               <div class="details-btn">
-                <button onclick="loadNewsDetails('${element._id}')" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#newsDetailsModal">Show Details</button>
+                <button onclick="loadNewsDetails('${
+                  element._id
+                }')" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#newsDetailsModal">Show Details</button>
               </div>
        </div>
     </div>`;
-  displayNews.appendChild(newsDiv);
-  loader(false);
-  })
-}
+      displayNews.appendChild(newsDiv);
+    });
+    loader(false);
+  }
+};
 
 // Load News Details By Modal
-const loadNewsDetails = news_id => {
+const loadNewsDetails = (news_id) => {
   fetch(`https://openapi.programming-hero.com/api/news/${news_id}`)
-  .then(res => res.json())
-  .then(data => displayNewsDetails(data.data[0]))
-  .catch(error => console.log(error));
-}
+    .then((res) => res.json())
+    .then((data) => displayNewsDetails(data.data[0]))
+    .catch((error) => console.log(error));
+};
 // Display News Details By Modal
-const displayNewsDetails = news => {
+const displayNewsDetails = (news) => {
   const modalTitle = document.getElementById("newsDetailsModalLabel");
   const modalDetails = document.getElementById("modal-details");
   const modalAuthor = document.getElementById("author");
   const modalPublishDate = document.getElementById("publish-date");
   const modalImg = document.getElementById("modal-img");
-  modalPublishDate.textContent = `Published Date: ${news.author.published_date ? news.author.published_date : "No Author Found!"}`
-  modalAuthor.textContent = `Author Name: ${news.author.name ? news.author.name : "No Author Found!"}`;
+  modalPublishDate.textContent = `Published Date: ${
+    news.author.published_date ? news.author.published_date : "No Author Found!"
+  }`;
+  modalAuthor.textContent = `Author Name: ${
+    news.author.name ? news.author.name : "No Author Found!"
+  }`;
   modalImg.src = `${news.image_url ? news.image_url : "No image found!"}`;
-  modalDetails.textContent = news.details.slice(0,200);
+  modalDetails.textContent = news.details.slice(0, 200).concat("...");
   modalTitle.textContent = news.title;
-}
+};
 
 // Spinners Function
-const loader =(isLoading) => {
+const loader = (isLoading) => {
   const spinners = document.getElementById("loader");
-  if(isLoading){
+  if (isLoading) {
     spinners.classList.remove("d-none");
-  } else{
+  } else {
     spinners.classList.add("d-none");
   }
-}
+};
 loadCategories();
